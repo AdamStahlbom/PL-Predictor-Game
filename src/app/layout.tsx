ca57b707ee4323d0f5ac4ef5872d499f4d2d9062
Adam Ståhlbom <adamstalbom@gmail.com>
2026-08-09 13:21:13 +0200

    logo n stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,21 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar"; // <-- Importera här
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Premier League Tips",
   description: "Tippa PL med vännerna",
+  openGraph: {
+    title: "PL Tips",
+    description: "Tippa Premier League med dina vänner!",
+    url: "https://adamstahlbom.se",
+    siteName: "PL Tips",
+    locale: "sv_SE",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
